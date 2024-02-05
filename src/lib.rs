6a3a12b1d837c392a291b695882a4d615e08ea2f
jekdev1999 <jekdev1999@gmail.com>
2024-02-05 15:02:03 -0500

    added instruction added functionality movie review

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ use solana_program::{
     msg
 };
 
+pub mod instruction;
+use instruction::MovieInstruction;
+
+
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
@@ -13,10 +17,41 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult{
-    msg!("process_instruction()");
-    msg!("Hello, Solana Program!");
+    msg!("process_instruction() called");
+
+    // Unpack called
+    let instruction = MovieInstruction::unpack(instruction_data)?;
+    // Match against the data struct returned into `instruction` variable
+    match instruction {
+        MovieInstruction::AddMovieReview { title, rating, description } => {
+            // Make a call to `add_move_review` function
+            add_movie_review(program_id, accounts, title, rating, description)
+        }
+    }
+
+
+
 
     Ok(())
 }
 
 
+
+pub fn add_movie_review(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    title: String,
+    rating: u8,
+    description: String
+) -> ProgramResult {
+
+    msg!("add_movie_review() called");
+
+    // Logging instruction data that was passed in
+    msg!("Adding movie review...");
+    msg!("Title: {}", title);
+    msg!("Rating: {}", rating);
+    msg!("Description: {}", description);
+
+    Ok(())
+}
